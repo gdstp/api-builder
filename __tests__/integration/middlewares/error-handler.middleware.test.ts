11385125cab6ec35979d52037f8bbbe0329925c4
gdstp <gabriel.teles.pinto@gmail.com>
2025-09-19 23:17:21 -0300

    test: ensure error middleware handles prisma p2002 error

--- __tests__/integration/middlewares/error-handler.middleware.test.ts
+++ __tests__/integration/middlewares/error-handler.middleware.test.ts
@@ -4,16 +4,22 @@ import { beforeEach, describe, expect, it } from "vitest";
 import request from "supertest";
 import express from "express";
 import logger from "@/utils/logger";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
 beforeEach(() => {
   logger.level = "silent";
 });
 
 describe("ErrorHandlerMiddleware", () => {
+  let app: express.Application;
+
+  beforeEach(() => {
+    app = express();
+    app.use(express.json());
+  });
+
   it("should handle AppError", async () => {
     const error = new AppError("Not found test error", 404, "NOT_FOUND");
-    const app = express();
-    app.use(express.json());
     app.get("/test", () => {
       throw error;
     });
@@ -29,6 +35,30 @@ describe("ErrorHandlerMiddleware", () => {
       error: {
         message: error.message,
         code: error.code,
+      },
+    });
+  });
+
+  it("should handle Unique constraint failed", async () => {
+    const error = new PrismaClientKnownRequestError(
+      "Unique constraint failed",
+      {
+        code: "P2002",
+        clientVersion: "6.16.2",
+      },
+    );
+    app.get("/test", () => {
+      throw error;
+    });
+    app.use(withErrorHandler);
+
+    const result = await request(app).get("/test").send().expect(409);
+
+    expect(result.body).toEqual({
+      success: false,
+      error: {
+        code: "DUPLICATE_ENTRY",
+        message: "A record with this information already exists",
       },
     });
   });
