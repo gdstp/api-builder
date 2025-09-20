ccc37508f22f3682820c30bd2d41d71e35eb67e1
gdstp <gabriel.teles.pinto@gmail.com>
2025-09-20 12:41:07 -0300

    test: ensure TokenService throws if invalid argument is passed to generateRefreshToken

--- __tests__/unit/services/token.service.test.ts
+++ __tests__/unit/services/token.service.test.ts
@@ -3,17 +3,32 @@ import { AppError } from "@/utils/app-error";
 import { describe, expect, it } from "vitest";
 
 describe("TokenService", () => {
-  it("should generate a token", async () => {
+  it("should generate a access token", async () => {
     const tokenService = new TokenService();
     const token = await tokenService.generateAccessToken("1");
 
     expect(token).toBeDefined();
   });
 
-  it("should throw an error if user id is not provided", async () => {
+  it("should throw an error if user id is not provided to generate access token", async () => {
     const tokenService = new TokenService();
 
     await expect(tokenService.generateAccessToken("")).rejects.toEqual(
+      new AppError("User ID is required", 400, "USER_ID_REQUIRED"),
+    );
+  });
+
+  it("should generate a refresh token", async () => {
+    const tokenService = new TokenService();
+    const token = await tokenService.generateRefreshToken("1");
+
+    expect(token).toBeDefined();
+  });
+
+  it("should throw an error if user id is not provided to generate refresh token", async () => {
+    const tokenService = new TokenService();
+
+    await expect(tokenService.generateRefreshToken("")).rejects.toEqual(
       new AppError("User ID is required", 400, "USER_ID_REQUIRED"),
     );
   });
