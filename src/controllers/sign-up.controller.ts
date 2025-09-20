979311074ccf3f7866441221d61e1022c74110ab
gdstp <gabriel.teles.pinto@gmail.com>
2025-09-19 22:32:56 -0300

    fix: remove unreachable validation

--- src/controllers/sign-up.controller.ts
+++ src/controllers/sign-up.controller.ts
@@ -1,7 +1,6 @@
 import UserRepository from "@/repositories/user.repository";
 import { SignUpInput } from "@/schemas/sign-up.schema";
 import Encrypter from "@/services/encrypter.service";
-import { AppError } from "@/utils/AppError";
 
 export default async function SignUpController(input: SignUpInput) {
   const encrypter = new Encrypter();
@@ -14,10 +13,6 @@ export default async function SignUpController(input: SignUpInput) {
     email: input.email,
     password: hashedPassword,
   });
-
-  if (!user) {
-    throw new AppError("User not created", 500, "USER_NOT_CREATED");
-  }
 
   return {
     id: user.id,
