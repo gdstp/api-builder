a733e77c4167a09d3b7f736d13f9f91be1238942
gdstp <gabriel.teles.pinto@gmail.com>
2025-09-20 12:30:21 -0300

    feat: add token generation to SignInController

--- src/controllers/sign-in.controller.ts
+++ src/controllers/sign-in.controller.ts
@@ -1,11 +1,13 @@
 import UserRepository from "@/repositories/user.repository";
 import { SignInInput } from "@/schemas/sign-in.schema";
 import Encrypter from "@/services/encrypter.service";
+import TokenService from "@/services/token.service";
 import { AppError } from "@/utils/app-error";
 
 export default async function SignInController(input: SignInInput) {
   const userRepository = new UserRepository();
   const encrypter = new Encrypter();
+  const tokenService = new TokenService();
   const user = await userRepository.getUserByEmail(input.email);
 
   if (!user) {
@@ -21,5 +23,18 @@ export default async function SignInController(input: SignInInput) {
     throw new AppError("Invalid password", 401, "INVALID_PASSWORD");
   }
 
-  return user;
+  const token = await tokenService.generateToken(user.id);
+
+  if (!token) {
+    throw new AppError(
+      "Failed to generate token",
+      500,
+      "FAILED_TO_GENERATE_TOKEN",
+    );
+  }
+
+  return {
+    user,
+    token,
+  };
 }
