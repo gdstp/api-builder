ef93f39355a506dd7c60e36dbe76eebdc03240d0
gdstp <gabriel.teles.pinto@gmail.com>
2025-09-20 12:26:29 -0300

    test: ensure TokenService throws AppError if no userId is provided

--- __tests__/unit/services/token.service.test.ts
+++ __tests__/unit/services/token.service.test.ts
@@ -1,4 +1,5 @@
 import TokenService from "@/services/token.service";
+import { AppError } from "@/utils/app-error";
 import { describe, expect, it } from "vitest";
 
 describe("TokenService", () => {
@@ -7,5 +8,13 @@ describe("TokenService", () => {
     const token = await tokenService.generateToken("1");
 
     expect(token).toBeDefined();
+  });
+
+  it("should throw an error if user id is not provided", async () => {
+    const tokenService = new TokenService();
+
+    await expect(tokenService.generateToken("")).rejects.toEqual(
+      new AppError("User ID is required", 400, "USER_ID_REQUIRED"),
+    );
   });
 });
