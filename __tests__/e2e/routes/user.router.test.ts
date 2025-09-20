1107c3eac1b55c64759793eb9c5bfa461db8a410
gdstp <gabriel.teles.pinto@gmail.com>
2025-09-20 00:21:16 -0300

    test: ensure user router returns 409 if user email is already registered

--- __tests__/e2e/routes/user.router.test.ts
+++ __tests__/e2e/routes/user.router.test.ts
@@ -1,13 +1,20 @@
-import { afterEach, describe, expect, it } from "vitest";
+import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
 import { app } from "@/server";
 import request from "supertest";
 import { emptyDatabase } from "__tests__/helpers/emptyDatabase";
+import { logger } from "@/utils";
+
+beforeEach(() => {
+  logger.level = "silent";
+});
 
 afterEach(async () => {
   await emptyDatabase();
 });
 
 describe("UserRouter", () => {
+  const spyOnLoggerError = vi.spyOn(logger, "error");
+
   it("should create a user", async () => {
     const response = await request(app).post("/api/v1/user/sign-up").send({
       name: "John Doe",
@@ -37,5 +44,27 @@ describe("UserRouter", () => {
     expect(response.body.error.fields.confirmPassword).toBe(
       "confirmPassword is a required field",
     );
+    expect(spyOnLoggerError).toHaveBeenCalledOnce();
+  });
+
+  it("should return 409 if the user already exists", async () => {
+    const user = {
+      name: "John Doe",
+      email: "john.doe@example.com",
+      password: "123456789",
+      confirmPassword: "123456789",
+    };
+
+    await request(app).post("/api/v1/user/sign-up").send(user);
+    const response = await request(app).post("/api/v1/user/sign-up").send(user);
+
+    expect(response.status).toBe(409);
+    expect(response.body).toHaveProperty("error");
+    expect(response.body.success).toBe(false);
+    expect(response.body.error.code).toBe("DUPLICATE_ENTRY");
+    expect(response.body.error.message).toBe(
+      "A record with this information already exists",
+    );
+    expect(spyOnLoggerError).toHaveBeenCalledOnce();
   });
 });
