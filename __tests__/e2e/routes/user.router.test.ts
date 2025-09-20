3f19bb8cb966b35d834dc08b1c69ddfd193e920b
gdstp <gabriel.teles.pinto@gmail.com>
2025-09-20 00:17:13 -0300

    tests: ensure user router returns 400 if signup input is invalid

--- __tests__/e2e/routes/user.router.test.ts
+++ __tests__/e2e/routes/user.router.test.ts
@@ -20,4 +20,22 @@ describe("UserRouter", () => {
     expect(response.body).toHaveProperty("data");
     expect(response.body.success).toBe(true);
   });
+
+  it("should return a 400 error if the request body is invalid", async () => {
+    const response = await request(app).post("/api/v1/user/sign-up").send({
+      name: "John Doe",
+      email: "john.doe@example.com",
+      password: "123456789",
+    });
+
+    expect(response.status).toBe(400);
+    expect(response.body).toHaveProperty("error");
+    expect(response.body.success).toBe(false);
+    expect(response.body.error.code).toBe("INVALID_REQUEST");
+    expect(response.body.error.message).toBe("Invalid request input");
+    expect(response.body.error.fields).toHaveProperty("confirmPassword");
+    expect(response.body.error.fields.confirmPassword).toBe(
+      "confirmPassword is a required field",
+    );
+  });
 });
